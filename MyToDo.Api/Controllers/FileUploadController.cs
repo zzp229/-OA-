c152f03cf53b5720850abf1f5ae019aeaa0a214d
zzp229 <13556632276@163.com>
2024-04-01 19:13:46 +0800

    实现查看邮箱和附件的下载

--- MyToDo.Api/Controllers/FileUploadController.cs
+++ MyToDo.Api/Controllers/FileUploadController.cs
@@ -35,5 +35,39 @@ namespace MyToDo.Api.Controllers
 
             return Ok(new { filePath });
         }
+
+
+        [HttpGet]
+        [Route("download/{fileName}")]
+        public IActionResult DownloadFile(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return BadRequest("Filename is not specified.");
+            }
+
+            var uploadFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFiles");
+            var filePath = Path.Combine(uploadFolderPath, fileName);
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("File not found.");
+            }
+
+            var memoryStream = new MemoryStream();
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                stream.CopyTo(memoryStream);
+            }
+            // 重置流的位置到开始，以便正确地读取文件内容
+            memoryStream.Position = 0;
+
+            // 返回文件流
+            // 第一个参数是文件流
+            // 第二个参数是MIME类型，这里可以根据文件类型动态指定，例如"application/pdf"，这里使用"application/octet-stream"使其更通用
+            // 第三个参数是下载时显示的文件名
+            return File(memoryStream, "application/octet-stream", Path.GetFileName(filePath));
+        }
+
     }
 }
